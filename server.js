f25e1e487720bc89a83799817f604312068da6eb
victorcavada <victorcavada98@gmail.com>
2017-11-30 20:28:39 -0600

    server2

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
+app.use(function (req, res, next){
+
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+
+  }
+  else{
+    res.redirect('http://'+ req.hostname + req.url)
+  }
+
+});
+
+
+
 app.use(express.static('public'));
 
 app.listen(port, function () {
